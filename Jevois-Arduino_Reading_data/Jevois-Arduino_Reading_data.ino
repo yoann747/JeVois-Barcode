86af12090581b109acdfc118c2f48a4aee4be4d1
yoann747 <31611938+yoann747@users.noreply.github.com>
2018-05-29 09:49:20 +0200

    Update Jevois-Arduino_Reading_data.ino

--- Jevois-Arduino_Reading_data/Jevois-Arduino_Reading_data.ino
+++ Jevois-Arduino_Reading_data/Jevois-Arduino_Reading_data.ino
@@ -3,22 +3,7 @@
 #include <Hexapod_Lib.h>
 #include <Servo.h>
   // put your setup code here, to run once:
- /*
-  mySerial.begin(115200);
-  mySerial.setTimeout(1000); //Tx Led frequency 
-  pinMode(LEDPIN, OUTPUT);
-  digitalWrite(LEDPIN, LOW);
-  
-  mySerial.println("setpar serlog None");
-  mySerial.println("setpar serout Hard");
-    
-
-}
-*/
-
-
-
-
+ 
 #define LEDPIN 13
 #define mySerial Serial
 //SoftwareSerial mySerial(0,1); 
@@ -27,10 +12,6 @@
 // LED_BUILTIN
 #define INLEN 128
 char instr[INLEN + 1];
-
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -47,9 +28,6 @@ void setup() {
 }
 
 
-
-
-
 void loop() {
  
  digitalWrite(LEDPIN , LOW);
@@ -57,36 +35,13 @@ void loop() {
   instr[len] = 0;
  // mySerial.println(len);
  digitalWrite(LEDPIN , HIGH);
-  
-  
- //rep [INLEN + 1] = instr [INLEN + 1];
- //char * tok = strtok(instr, " \r\n");  
-//int state = 0; int targx = 0, targy = 0;
+
 
 mySerial.println(instr);
   
-     /*
-     while (tok)
- {
-switch (state)
-      {       case 0: if (strcmp(tok, "N2") == 0 || strcmp(tok, "N3") == 0 ) state = 1; else state = 1000; break;
-               case 1: 
-                      default: break; // Skip any additional tokens
-}
-}
-
-*/
-   
-
-   
-
-  
+ 
 
    }
  
 
  
- 
-  //mySerial.println(*tok);
-
-
